e95e4d7120ab69525dc80feeb57ab7e811233cf1
Joseph Carmer <jcarmer@mildthing.com>
2013-02-07 08:31:05 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Devblog::Application.config.secret_token = 'ed49c0ffadeecc907abdd056504553ef201df7d059d8bb907e0c2ef2370cc1b9e109577f146077222a10d24a6e0aa212114720bc575645727f1faf8321f685e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Devblog::Application.config.session_store :cookie_store, key: '_devblog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Devblog::Application.config.session_store :active_record_store
